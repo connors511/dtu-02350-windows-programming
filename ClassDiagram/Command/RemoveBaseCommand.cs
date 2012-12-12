909077ca0e4b2884fd8cafe5ba7e9e77e8ee94e8
joachim <jv@intox.dk>
2012-12-12 15:04:10 +0100

    remove unused assocs on class deletion

--- ClassDiagram/Command/RemoveBaseCommand.cs
+++ ClassDiagram/Command/RemoveBaseCommand.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Collections.ObjectModel;
 using ClassDiagram.Models.Entities;
 using ClassDiagram.Models;
+using ClassDiagram.Models.Arrows;
 
 namespace ClassDiagram.Command
 {
@@ -22,12 +23,41 @@ namespace ClassDiagram.Command
         public void Execute()
         {
             this._bases.Remove(_base);
-            
+            this.editAssoc();
         }
 
         public void UnExecute()  
         {
             this._bases.Add(_base);
+            this.editAssoc();
         }
+
+        private void editAssoc()
+        {
+            // Remove all current arrows
+            List<Base> removes = (from b in _bases
+                                  where b.GetType() != typeof(Entity) // Gets all arrows
+                                  select b).ToList();
+            removes.ForEach(x => _bases.Remove(x));
+            // Re-add all arrows
+            List<Base> bs = (from b in _bases
+                             where b.GetType() == typeof(Entity)
+                             select b).ToList();
+            // We can do this, because all arrows has been removed
+            bs.ForEach(x =>
+            {
+                ((Entity)x).Properties.ToList().ForEach(y =>
+                {
+                    var z = (from b in _bases
+                             where b.Name == y.Type
+                             select b).ToList();
+                    z.ForEach(i =>
+                    {
+                        _bases.Add((Base)new Association() { Start = (Entity)x, End = (Entity)i });
+                    });
+                });
+            });
+        }
+
     }
 }
